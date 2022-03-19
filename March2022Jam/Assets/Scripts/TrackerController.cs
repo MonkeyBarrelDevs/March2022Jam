c1c29c65ee3dfe314e58331f9cef302fb74b2f0c
Ethan Boggs <ethanbeez@gmail.com>
2022-03-19 15:43:17 -0600

    Tracker projectile working!

---
+++ March2022Jam/Assets/Scripts/TrackerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackerController : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 5;
+    private Rigidbody2D rb;
+    
+    public float rotateSpeed = 200;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 direction = (Vector2) target.position - rb.position;
+        direction.Normalize();
+        float rotationAmount = Vector3.Cross(direction, transform.up).z;
+        rb.angularVelocity = -rotationAmount * rotateSpeed;
+        rb.velocity = transform.up * speed;
+    }
+}
