0e48e7ae98781da1d8d203b4a6840f57888313ec
BRANDON WALTERS <u1199080@utah.edu>
2022-03-19 15:42:51 -0600

    Shake/flip rotate now possible

--- March2022Jam/Assets/Scripts/SpinShooter.cs
+++ March2022Jam/Assets/Scripts/SpinShooter.cs
@@ -6,16 +6,31 @@ public class SpinShooter : MonoBehaviour
 {
     public GameObject shooterObject;
     public float speedOfRotation;
+    public float timeBetweenShakes;
+    public bool shakeRotate;
+
+    private float timeSinceShot;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        timeSinceShot = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (shakeRotate)
+        {
+            timeSinceShot += Time.deltaTime;
+
+            if (timeSinceShot > timeBetweenShakes)
+            {
+                speedOfRotation = -speedOfRotation;
+                timeSinceShot = 0f;
+            }
+        }
+        
         shooterObject.transform.Rotate(0.0f, 0.0f, speedOfRotation, Space.World);
     }
 }
